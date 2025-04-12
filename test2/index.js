26d8df9bf8f33404bedb8d027fcd2a9bc54f4b8f
Wei <asabisa52@gmail.com>
2025-04-12 15:04:36 +0800

    finish

--- test2/index.js
+++ test2/index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+function filterImages(category) {
+    const allImages = document.querySelectorAll('.gallery img');
+
+    allImages.forEach(img => {
+        if (category === 'all') {
+            img.style.display = 'block';
+        } else {
+            if (img.classList.contains(category)) {
+                img.style.display = 'block';
+            } else {
+                img.style.display = 'none';
+            }
+        }
+    });
+}
